bd8dc3485f0778f4c05728caf980210c50470253
Benjamin Schaff <benjamin.schaff@maine.edu>
2017-10-17 15:30:33 -0400

    blink led for new pin layout

--- avr_test/main.c
+++ avr_test/main.c
@@ -1,15 +1,16 @@
+#define F_CPU 1000000
+
 #include <stdio.h>
 #include <avr/io.h>
-#define F_CPU 1000000
 #include <util/delay.h>
 
-
 int main() {
-	DDRB |= (1<<0); // set PB0/D0  to output
+
+	DDRA |= (1<<4); // set PA4 to output
 	while (1) {
-		PORTB |= (1<<0);
-		_delay_ms(250);
-		PORTB &= ~(1<<0);
-		_delay_ms(250);
+		PORTA |= (1<<4);
+		_delay_ms(500);
+		PORTA &= ~(1<<4);
+		_delay_ms(500);
 	}
 }
