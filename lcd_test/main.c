7cea4f2c8bc263f6939811fa3eace27353683e93
Daniel Paradis <dan.paradis@maine.edu>
2017-10-25 18:07:05 -0400

    Some non-working lcd code

--- lcd_test/main.c
+++ lcd_test/main.c
@@ -3,40 +3,13 @@
 #define F_CPU 2000000
 #include <util/delay.h>
 
+#include "lcd.h"
 
-int main() {
-}
-
-void command(char c)
+int main()
 {
-	DDRA |= (1 << 5); // set PA5 to output
-	PORTA &= ~(1 << 5); // clock enable falling edge
-}
+	lcd_init();
+	lcd_xy(2, 0);
+ 	lcd_puts("Test");
 
-
-void write(char c)
-{
-	
-	DDRA |= (1 << 5); // set PA5 to output
-	PORTA &= ~(1 << 5); // clock enable falling edge
-}
-
-void init()
-{
-	DDRA |= (1 << 5); // set PA5 to output
-	PORTA &= ~(1 << 5); // clock enable falling edge
-
-	// three wakeup calls
-	_delay_ms(50);
-	command(0x30);
-	_delay_ms(5);
-	command(0x30);
-	_delay_us(160);
-	command(0x30);
-	_delay_us(160);
-
-	command(0x38); // set 8bit/2bit line
-	command(0x10); // set cursor
-	command(0x0C); // display ON and cursor ON
-	command(0x06); // Entry mode set
+  	while (1);
 }
