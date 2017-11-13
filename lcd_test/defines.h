5a17ee15c56907015166e4c9e3d603e765d080a6
Daniel Paradis <dan.paradis@maine.edu>
2017-11-12 22:35:34 -0500

    Make some test code for the lcd and fixed some errors in the files

---
+++ lcd_test/defines.h
@@ -0,0 +1,6 @@
+#ifndef asdfasdf_defines
+#define asdfasdf_defines
+
+#define F_CPU 8000000UL
+
+#endif
--- lcd_test/main.c
+++ lcd_test/main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <avr/io.h>
-#define F_CPU 2000000
 #include <util/delay.h>
 
-#include "lcd.h"
+
+#include "defines.h"
+#include "hd44780_settings.h"
+#include "hd44780.h"
 
 int main()
 {
 	lcd_init();
-	lcd_xy(2, 0);
- 	lcd_puts("Test");
-
-  	while (1);
+	
+  	while (1) {
+		lcd_clrscr();
+		lcd_home();
+		lcd_puts("Hello World!");
+		lcd_goto(0x40);
+		lcd_puts("Second line");
+		lcd_goto(0x14);
+		lcd_puts("Third line");
+		lcd_goto(0x54);
+		lcd_puts("Fourth line");
+		_delay_ms(50);
+	}
 }
