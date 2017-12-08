878af0b03fa4f5fc6e081dc1c90176383d3d84b3
Daniel Paradis <dan.paradis@maine.edu>
2017-12-07 19:49:07 -0500

    Made some screens

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include "uart.h"
+#include "ui.h"
 #include "i2c.h"
 #include "sensors.h"
 #include "hd44780_settings.h"
@@ -16,17 +17,40 @@
 FILE uart_strm = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 
 char buf[20];
+SCREEN ui[4];
+SCREEN main_menu {
+	lines = {"Sensors", "Saved Data", "Settings"};
+	line_values = {0, 0, 0};
+	max_values = {1, 1, 1};
+	length = 3;
+	screen_index = 0;
+	curser_index = 0;
+};
 
+SCREEN sensor_readouts {
+	lines = {"Temperature", "Pressure", "Wind Speed", "Humidity", "Wind Chill", "Dew Point", "Humidex"};
+	line_values = {0, 0, 0, 0, 0, 0, 0};
+	max_values = {6, 6, 6, 6, 6, 6, 6};
+	length = 7;
+	screen_index = 0;
+	curser_index = 0;
+};
 
-void print_sensors()
+SCREEN settings {
+	lines = {"Temp Units", "Press Units", "Wind Units"};
+	line_values = {0, 0, 0};
+	max_values = {1, 2, 3};
+	length = 3;
+	screen_index = 0;
+	curser_index = 0;
+};
+
+void sensor_readout_update()
 {
 	int16_t temp = get_temp();
 	unsigned long pressure = get_pressure();
 	unsigned int wind = get_wind();
 	uint16_t humid = get_humid();
-
-	lcd_clrscr();
-	lcd_home();
 
 	sprintf(buf, "Temp: %d.%d C", temp/10, abs(temp%10)); // convert temp to a string
 	lcd_puts(buf); // print temperature to first line of screen
@@ -38,10 +62,6 @@ void print_sensors()
 	lcd_goto(0x14);
 	sprintf(buf, "Speed: %u.%u mph", wind/10, wind%10);
 	lcd_puts(buf);
-
-//	lcd_goto(0x54);
-//	sprintf(buf, "Input Cap: %ld", aaaaa);
-//	lcd_puts(buf);
 }
 
 
