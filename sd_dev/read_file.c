166be82225e4a2a474cae370c855ea466c7ec1c9
Benjamin Schaff <benjamin.schaff@maine.edu>
2017-12-08 11:31:36 -0500

    fat init function can correctly read required info from arbitrary fat16 filesystem images

--- sd_dev/read_file.c
+++ sd_dev/read_file.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
+#define DEBUG
+
 #include <stdlib.h>
 #include <stdint.h>
+#ifdef DEBUG
+	#include <stdio.h>
+#endif
 
-struct f16_part_table{
+struct f16_part_table { // sizeof()=16B
 	uint8_t first_byte;
 	uint8_t start_chs[3];
 	uint8_t part_type;
@@ -11,36 +15,38 @@ struct f16_part_table{
 	uint32_t length_sectors;
 } __attribute((packed));
 
-struct f16_boot_sector {
+
+struct f16_boot_sector { //sizeof()=24B
 	//uini8_t jmp[3];
 	//char oem[8];
 	uint8_t start_padding[11]; // for allignment purposes
-	uint16_t sect_sz;
+	uint16_t sector_size;
 	uint8_t sectors_per_cluster;
 	uint16_t reserved_sectors;
-	uint8_t nfats;
+	uint8_t num_fats;
 	uint16_t root_dir_entries;
 	uint16_t total_sectors_short; // if zero, later field is used
 	uint8_t media_descriptor;
-	uint16_t fat_sz_sects;
-	/*
+	uint16_t fat_size_sects;
+	/* Not needed for SD card
 	uint16_t sectors_per_track;
 	uint16_t nheads;
 	uint32_t hidden_sectors;
-	uint32_t total_sectors_long;
+	uint32_t total_sectors;
 	
-	uint8_t drive_number;
+	uint8_t drive_num;
 	uint8_t current_head;
-	uint8_t boot_signature;
+	uint8_t boot_sig;
 	uint32_t volume_id;
-	char volume_label[11];
-	char fs_type[8];
-	char boot_code[448];
-	uint16_t boot_sector_signature;*/
+	uint8_t volume_label[11];
+	uint8_t fs_type[8];
+	uint8_t boot_code[448];
+	uint16_t boot_sect_sig;
+	*/
 } __attribute((packed));
 
 
-struct f16_entry {
+struct f16_entry { // sizeof()=32B
 	uint8_t filename[8];
 	uint8_t ext[3];
 	uint8_t attributes;
@@ -52,20 +58,22 @@ struct f16_entry {
 } __attribute((packed));
 
 
-uint8_t f16_rw_buffer[512]; // read/write buffer used by fat16 disk io
+uint8_t f16_r_buffer[32];	// read buffer used by fat16 disk io
+uint8_t f16_w_buffer[512];	// write buffer for fat16 
 
 /*
  * Minimum saved data for fat 16 operation
  */
-struct f16_state {
+struct {
 	uint32_t fat_start; // = 0x400C00;		// Addr of FAT
+	uint32_t root_start; //=?				// Addr of root dir
 	uint32_t data_start; // = 0x440000;		// Addr of data sector
 	uint32_t sect_per_cluster; // = 128;	// Number of sectors (512 bytes) per cluster
 	uint16_t file_start_cluster;			// Index of starting cluster of file
 	uint16_t file_cluster; // = 5;			// Index of file cluster being read
 	uint32_t file_size; // = 131072;		// Size of file being read
 	
-} __attribute((packed)); 
+} f16_state;
 
 FILE *fin;
 
@@ -83,7 +91,7 @@ void f16_seek(uint32_t offset)
 uint16_t f16_read(uint16_t count)
 {
 	// TODO, limit size to <512?
-	return (uint16_t)fread(f16_rw_buffer, 1, count); //TODO read from SD
+	return (uint16_t)fread(f16_r_buffer, 1, count, fin); //TODO read from SD
 }
 
 
@@ -93,50 +101,100 @@ uint16_t f16_write(uint16_t count)
 	return 0;
 }
 
-/* Reads partition table on disk.  Goes to first partition found.
+/* 
+ * Reads partition table on disk.  Goes to first partition found.
  * Saves requried data from bootsector and partition table.
  * Sets read to start of root directory
  */
 int f16_init()
 {
-	struct f16_part_table ptable[4];
-	struct f16_boot_sector bootsect;
-	struct f16_entry entry;
+	int i;
+	uint32_t start_sector;
+	struct f16_part_table *ptable = (struct f16_part_table *)((void *)f16_r_buffer);
+	struct f16_boot_sector *bootsect = (struct f16_boot_sector*)((void *)f16_r_buffer);
+	struct f16_entry *entry = (struct f16_entry *)(struct f16_entry *)((void *)f16_r_buffer);
 
 	f16_seek(0x1BE); // go to partition table start
-	fread(ptable, sizeof(struct f16_part_table), 4, fin); // read all four entries
-	
-	for(i=0; i<4; i++) {		
-		if(ptable[i].part_type == 4 || ptable[i].part_type == 6 ||
-		   ptable[i].part_type == 14) {
+
+	// Read partition table entries into buffer on at a time and find first valid one
+	for ( i = 0; i < 4; i++ ) {	
+		f16_read(sizeof(struct f16_part_table)); // read table entry
+
+		// check if partition type, break if it's valid
+		if (ptable->part_type == 4 
+			|| ptable->part_type == 6
+			|| ptable->part_type == 14) {
+#ifdef DEBUG
 			printf("FAT16 filesystem found from partition %d\n", i);
+#endif
 			break;
 		}
 	}
 	
-	if(i == 4) {
+
+	if (i == 4) { // no valid fat16 partition found, return error
+#ifdef DEBUG
 		printf("No FAT16 filesystem found, exiting...\n");
+#endif
 		return -1;
 	}
 
-	fseek(fin, 512 * ptable[i].start_sector, SEEK_SET);
-	fread(&bootsect, sizeof(struct f16_boot_sector), 1, fin);
+	// Need to save this before loading bootsector into buffer
+	start_sector = ptable->start_sector; 
+
+	// Read boot sector into buffer
+	f16_seek(512 * ptable->start_sector);
+	f16_read(sizeof(struct f16_boot_sector));
 	
+#ifdef DEBUG
 	printf("Now at 0x%lX, sector size %d, FAT size %d sectors, %d FATs\n\n", 
-		   ftell(fin), bootsect.sect_sz, bootsect.fat_sz_sects, bootsect.nfats);
-		   
-	fseek(fin, (bootsect.reserved_sectors-1 + bootsect.fat_sz_sects * bootsect.nfats) *
-		  bootsect.sect_sz, SEEK_CUR);
-			
-	for( i = 0; i < bootsect.root_dir_entries; i++ )
-	{
-		fread(&entry, sizeof(entry), 1, fin);
-		print_file_info(&entry);
+		   ftell(fin), bootsect->sector_size, bootsect->fat_size_sects, bootsect->num_fats);
+#endif		   
+	
+	// if sector size isn't 512, it won't work with SD card as implimented
+	if (bootsect->sector_size != 512) {
+		return -1;
+	}
+
+	// save important values to global f16_state struct
+	f16_state.fat_start = (start_sector + bootsect->reserved_sectors) * 512;
+	f16_state.root_start = f16_state.fat_start + bootsect->fat_size_sects
+			* bootsect->num_fats * 512;
+	f16_state.data_start = f16_state.root_start
+			+ bootsect->root_dir_entries * sizeof(struct f16_entry);
+
+#ifdef DEBUG
+	printf("Fat start:\t%X\n", f16_state.fat_start);
+	printf("Root start:\t%X\n", f16_state.root_start);
+	printf("Data start:\t%X\n", f16_state.data_start);
+#endif
+	
+
+	// Go to start of root dir
+	f16_seek(f16_state.root_start);
+
+#ifdef DEBUG
+	printf("\nMoved to start of Root directory, now at 0x%lX\n", ftell(fin));
+#endif
+
+
+
+
+//////TODO ELSEWEHERER			
+	for ( i = 0; i < bootsect->root_dir_entries; i++ ) {
+		f16_read(sizeof(entry));
+//	int j = 0;
+		//TODO something with entry, find filenamme?  move this to openfile?
+		//for (j
+		//if (entry->filename[0] != 0x00 || entry-> 
+		//printf("
 	}
 	
+#ifdef DEBUG
 	printf("\nRoot directory read, now at 0x%lX\n", ftell(fin));
-
+#endif
 	
+
 	return 0;
 }
 
@@ -149,14 +207,13 @@ void f16_read_file(uint16_t bytes)
 {
 	
 
-	f16_seek(data_start + (start_cluster-2)*cluster_size, SEEK_SET);
+	f16_seek(f16_state.data_start + (f16_state.file_start_cluster-2)*f16_state.sect_per_cluster*512);
 	f16_read(bytes);
 
 }
 
 
 int main(int argc, char **argv) {
-	int i;
 
 	if (argc > 1) {
 		fin = fopen(argv[1], "rb");
@@ -170,9 +227,8 @@ int main(int argc, char **argv) {
 	f16_read_file(32);
 
 
-	f16_rw_buffer[31] = 0;	
-	f16_rw_buffer[512 - 1] = 0;
-	printf("%s",buffer);
+	f16_r_buffer[sizeof(f16_r_buffer)-1] = 0;	
+	printf("%s\n",f16_r_buffer);
 
 
 	fclose(fin);
