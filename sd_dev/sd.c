c3178db762cf3c131b8abaf3c368724f961ee94c
Benjamin Schaff <benjamin.schaff@maine.edu>
2017-12-09 17:07:41 -0500

    add functions for sending specific SD commands, updated test prog accordingly

--- sd_dev/sd.c
+++ sd_dev/sd.c
@@ -2,22 +2,105 @@
 #include <avr/io.h>
 #include "sd.h"
 
-void sd_init()
-{
+#define SD_DEBUG // if defined, PA4 is toggled the same as the CS line
 
+#define RETRY_COUNT 10
+
+/*
+ *	Brings the SPI circuit select line low (active)
+ */
+void cs_enable()
+{
+	PORTB &= ~(1<<4);   // CS Enable
+#ifdef SD_DEBUG
+	// for debugging also, also toggling led pin which is broken out
+	PORTA &= ~(1<<4);
+#endif
+
+}
+
+/*
+ *	Brings the SPI circuit select line high (inactive)
+ */
+void cs_disable()
+{
+	PORTB |= (1<<4);    // CS Disable
+#ifdef SD_DEBUG
+	// for debugging bus, also toggling led pin which is broken out
+	PORTA |= (1<<4);
+#endif
+}
+
+/*
+ *	Performes SPI pin and clock initialization
+ */
+void spi_init(){
 	// Setting up SPI pins
 	DDRB |= (1<<4) | (1<<5) | (1<<7);	// Set CS (PB4), MOSI (PB5),and SCK (PB7) output.  
 	DDRB &= ~(1<<6);	// and MISO (PB6) as input
-	PORTB |= (1<<6);	// Pullup on MISO
-	
+	PORTB |= (1<<6);	// Pullup on MISO	
 	// Enable SPI, master mode, 
-	SPCR = (1<<SPE) | (1<<MSRR) | (1<<SPR0) | (SPR1);
+	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0) | (SPR1);
+}
+
+/*
+ *	Brings SD card out of idle, sets proper read/write block sizes
+ */
+void sd_init()
+{
+	int i;
+	spi_init(); // SD card needs SPI working first
+
+
+	cs_disable(); // Begin communication
+
+	// send 80 clock cycles so card can sync up
+	for (i = 0; i < 10; i++) {
+		SPI_write_byte(0xFF);
+	}
+
+
+	for (i = 0; i < RETRY_COUNT; i++) {
+		if (SD_send_command(0x40, 0x00000000, 0x95, 8)
+
+	}	
+
+	
+}
+
+/*
+ *	Sends a byte out over the spi bus and returns the bytes recieved.
+ * 	Blocks until tranmission completes.
+ */
+uint8_t SPI_write_byte(uint8_t byte) 
+{
+	SPDR = byte;					// load byte into spi data register
+	while (!(SPSR & (1<<SPIF)));	// wait until transmission completes
+	return SPDR;					// return byte recieved
 }
 
 
-uint8_t SPI_write_char(uint8_t byte) 
+void sd_send_command(
+		uint8_t cmd,		// Command
+		uint32_t arg, 		// 32 bit argument
+		uint8_t crc, 		// CRC for cmd and arg
+		uint8_t read_bytes, // How many bytes to read back
+		uint8_t *read_buf)	// Buffer to store read bytes in
 {
-	SPDR = byte;
-	while (!(SPSR & (1<<SPIF)));
-	return SPDR;
+	uint8_t i;
+	cs_enable(); // Begin SPI transmision
+
+	SPI_write_byte(cmd);
+	SPI_write_byte((arg>>24) & 0xFF);	// send MSB of argument
+	SPI_write_byte((arg>>16) & 0xFF);	// and next MSB
+	SPI_write_byte((arg>>8) & 0xFF);	// and next...
+	SPI_write_byte(arg & 0xFF);			// and LSB
+	SPI_write_byte(crc);
+
+	// read response.
+	for (i = 0; i < read_bytes; i++) {
+		read_buf[i] = SPI_write_byte(0xFF);
+	}
+
+	cs_disable(); // End SPI transmission
 }
--- sd_dev/sd.h
+++ sd_dev/sd.h
@@ -1,10 +1,18 @@
 #ifndef asdfasdf_sd
 #define asdfasdf_sd
 
-#include <sdtint.h>
+#include <stdint.h>
+
+void cs_enable();
+
+void cs_disable();
 
 void sd_init();
 
+uint8_t SPI_write_byte(uint8_t bytes);
+
+void sd_send_command(uint8_t cmd, uint32_t arg, uint8_t crc,
+					 uint8_t read_bytes, uint8_t *read_buf);
 
 #endif
 
