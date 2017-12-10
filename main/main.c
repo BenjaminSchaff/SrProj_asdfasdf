6d0a65ca0e551056264db98b9246b425049a4e96
Daniel Paradis <dan.paradis@maine.edu>
2017-12-09 19:33:51 -0500

    uploading button test and ui code for actual device.  Please Jesus....

--- main/main.c
+++ main/main.c
@@ -16,8 +16,7 @@
 
 FILE uart_strm = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 
-char buf[20];
-SCREEN ui[3];
+char lcd_buf[21];
 
 void sensor_readout_update()
 {
@@ -46,19 +45,48 @@ void init()
 	uart_init();
 	i2c_init();
 	sensor_init();
-	ui_init(ui);
 }
 
 int main() 
 {
+	int button, current_screen_index = 0;
+	int i;
+	SCREEN ui[3];
+
 	init();
+	ui_init(ui);
 
 	while (1) {
 		PORTA ^= (1<<4); // Blinking LEDs are great. Also tells how fast main loop completes.
 
+		print_screen(&ui[current_screen_index]); // update screen
+
+		for (i = 1; i < 7; i++) { // buttons are labeled 1-6
+			if (((~PINC) << (1 + i)) & 0x01) {
+				if (i == 6) { // if the button is back screen, go home
+					current_screen_index = 0; // 0 is the home screen
+				} else if (i == 3) { // if you press the goto screen button
+					if (current_screen == 0) { // and you are at the home screen
+						if (ui[0].curser_index == 0) // and your curser is over the sensors option
+							current_screen_index = 1; // goto sensors
+						else if (ui[0].curser_index == 2) // and your curser is over the settings option
+							current_screen_index = 2; // goto settings
+					}
+				}
+				update_screen_state(i, current_screen, &ui[current_screen_index]); // update screens with new values
+				break;
+			}
+		}
 		update_sensors();
+		switch (current_screen_index) {
+		case 1:
+			update_sensor_strings(&ui[1], &ui[2], lcd_buf);
+			break;
+		case 2:
+			update_settings_strings(&ui[2]);
+			break;
+		}
 		
-		sensor_readout_update();
 		_delay_ms(950);
 
 		//fprintf(&uart_strm, "Temperature: %f C\n", f_temp);
